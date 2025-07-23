4bf2aabf740b908452788f9cf469c2690c3c6afc
Ablebil <hafizhfaiq07@gmail.com>
2025-07-23 07:59:35 +0700

    feat: add better logging

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,9 +5,40 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import routes from "./routes/routes";
+import { errorHandler } from "./middlewares/error-handler";
+import logger from "./utils/logger";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const logDirectory = path.join(__dirname, "..", "..", "logs");
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+const morganStream = fs.createWriteStream(
+  path.join(logDirectory, "access.log"),
+  { flags: "a" }
+);
+app.use(
+  morgan("combined", {
+    stream: morganStream,
+    skip: () => process.env.NODE_ENV === "test",
+  })
+);
+app.use(
+  morgan("dev", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+    skip: () => process.env.NODE_ENV === "test",
+  })
+);
 
 app.use(
   cors({
@@ -16,12 +47,11 @@ app.use(
   })
 );
 app.use(helmet());
-app.use(morgan("dev"));
 app.use(express.json());
-
 app.disable("x-powered-by");
 
 app.use("/api/v1", routes);
+app.use(errorHandler);
 
 const PORT = parseInt(process.env.PORT || "3000");
 const MONGO_URI = process.env.MONGO_URI;
@@ -29,13 +59,13 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose
   .connect(MONGO_URI!)
   .then(() => {
-    console.log("Connected to MongoDB");
+    logger.info("Connected to MongoDB");
 
     app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server is running on port ${PORT}`);
+      logger.info(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error(err);
+    logger.error(err);
     process.exit(1);
   });
